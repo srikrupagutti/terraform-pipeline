0debcb8d38919c19a6c12ed6659fa2c83bdc5647
srikrupagutti <156899186+srikrupagutti@users.noreply.github.com>
2024-05-06 11:51:11 -0500

    Update vm.tf

--- modules/vm.tf
+++ modules/vm.tf
@@ -15,7 +15,7 @@ resource "random_id" "suffix" {
 
 resource "azurerm_virtual_network" "vnet" {
   name                = "vnet-${var.env}-${random_id.suffix.hex}"
-  env                 = var.env
+  location            = var.loaction
   address_space       = ["10.0.0.0/16"]
   resource_group_name = var.rg_name
 }
